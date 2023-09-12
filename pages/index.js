bf8d507827a7cd6f9c3ca7b6d71220d0feeeae8d
vasaniyakush <vasaniyakush@gmail.com>
2023-09-12 08:47:32 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,24 @@
-import React, { useReducer, useEffect, useState } from "react";
+import React from 'react';
+import useFetch from '../components/useFetch';
 
-const initialState = { hotels: [], filteredHotels: [] };
+function Home() {
+  const { data, loading, error } = useFetch("https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?limit=10&page=1");
 
-function reducer(state, action) {
-  switch (action) {
-    case "FETCH_SUCCESS":
-     
-    case "FILTER":
-     
-    default:
-   
-  }
-}
-export default function Home() {
-   const [state, dispatch] = useReducer(reducer, initialState);
- 
-
-
-
-  useEffect(() => {
-    dispatch({ type: "FILTER"});
-  });
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Enter city name"
-      />
-     
-        <p >hotel_name</p>
-     
+    <div>
+      <h1>Movies</h1>
+      {/* {document.write(data)} */}
+      <ul>
+      {data && data.map(item => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+
+      </ul>
     </div>
   );
-  }
-  
+}
+
+export default Home;
